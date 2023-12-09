80d063221dc3798520b21efd763ad50fdd659b5b
Kennedy Cesar Pimentel <104803647+KennedyDvk@users.noreply.github.com>
2023-12-08 23:25:55 -0300

    Primary commit

---
+++ src/main/java/br/com/clinimed/ClinimedApplication.java
@@ -0,0 +1,13 @@
+package br.com.clinimed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinimedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinimedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/clinimed/ClinimedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clinimed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinimedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
